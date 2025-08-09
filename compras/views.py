8db385016a8e6e3ed78085b7b84675ec3d0d2a3d
nenriquezc <nenriquezc@gmail.com>
2025-08-09 11:45:31 +0100

    recuperado proyecto cong igual a vscde

--- compras/views.py
+++ compras/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
+from .models import Compra
 from django.shortcuts import render
 
+
+# Create your views here.
+def index(request):
+    # productos = Producto.objects.all()
+    return HttpResponse("esta es la view de compras yu compras productos")
 # Create your views here.
--- suministros/urls.py
+++ suministros/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', include('inicio.urls')),
+    path('', include('inventario.urls')),  # Esto incluye las URLs de la app # 👈 Home en raíz
+    path("compras/", include("compras.urls")),
+    path("estadisticas/", include("estadisticas.urls")),
+    path("inventario/", include("inventario.urls")),
+    path("ventas/", include("ventas.urls")),
 ]
